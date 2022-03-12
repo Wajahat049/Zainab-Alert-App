e299952cc738a8dfd78b4289924fb04067007464
Aleezah Ikram <64039135+Aleezah1429@users.noreply.github.com>
2022-03-13 00:14:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,11 +20,14 @@ const customStyles = {
 }
 
 function App() {
-  // Modal States
+  // Modal States For Missing Child
   let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
+  
+  // Modal States For Found Child
+  const [modalIsOpen2, setIsOpen2] = React.useState(false);
 
-  // Modal
+  // Modal For Missing Child
   function openModal() {
     setIsOpen(true);
   }
@@ -36,6 +39,21 @@ function App() {
 
   function closeModal() {
     setIsOpen(false);
+  }
+
+
+  // Modal For Found Child
+  function openModal2() {
+    setIsOpen2(true);
+  }
+
+  function afterOpenModal2() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal2() {
+    setIsOpen2(false);
   }
 
 
@@ -51,7 +69,7 @@ function App() {
             Report Missing Child
           </Row>
         </Col>
-        <Col className='col card'>
+        <Col className='col card' onClick={openModal2}>
           <img height={"70%"} width={"40%"} src={found_child} alt="find chid icon" />
           <Row className='img_text'>
             Report Found Child
@@ -67,7 +85,7 @@ function App() {
 
 
 
-      {/* Modal */}
+      {/* Modal for Missing Child*/}
       <Modal
         isOpen={modalIsOpen}
         onAfterOpen={afterOpenModal}
@@ -79,7 +97,7 @@ function App() {
         <h1 className='cancel_icon' onClick={closeModal}><MdCancel /></h1>
 
         <h2 className='modal_text'>Report Missing Child</h2>
-        <Form style={{ fontSize: "120%" }}>
+        <Form>
           <Form.Group className="mb-3 input_field" controlId="ChildPic">
             <Form.Label>Recent Child Picture</Form.Label>
             <Form.Control type="file" multiple="multiple" name='ChildPic' />
@@ -101,12 +119,53 @@ function App() {
             <Form.Control type="text" placeholder="Gender" name='Gender' />
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button variant="success" type="submit">
             Submit
           </Button>
         </Form>
       </Modal>
 
+
+
+
+      {/* Modal for Found Child*/}
+      <Modal
+        isOpen={modalIsOpen2}
+        onAfterOpen={afterOpenModal2}
+        onRequestClose={closeModal2}
+        style={customStyles}
+        className="ModelContent"
+      >
+        <h1 className='cancel_icon' onClick={closeModal2}><MdCancel /></h1>
+
+        <h2 className='modal_text'>Report Found Child</h2>
+        <Form>
+          <Form.Group className="mb-3 input_field" controlId="Name">
+            <Form.Label> Name</Form.Label>
+            <Form.Control type="text" placeholder=" Name" name='Name' />
+          </Form.Group>
+          <Form.Group className="mb-3 input_field" controlId="Father_Name">
+            <Form.Label>Father Name</Form.Label>
+            <Form.Control type="text" placeholder="Father Name" name='Father_Name' />
+          </Form.Group>
+          <Form.Group className="mb-3 input_field" controlId="Age">
+            <Form.Label>Age</Form.Label>
+            <Form.Control type="text" placeholder="Age" name='Age' />
+          </Form.Group>
+          <Form.Group className="mb-3 input_field" controlId="Gender">
+            <Form.Label>Gender</Form.Label>
+            <Form.Control type="text" placeholder="Gender" name='Gender' />
+          </Form.Group>
+          <Form.Group className="mb-3 input_field" controlId="Area_Found">
+            <Form.Label>Area Found</Form.Label>
+            <Form.Control type="text" placeholder="Area" name='Area_Found' />
+          </Form.Group>
+
+          <Button variant="success" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Modal>
 
 
 
